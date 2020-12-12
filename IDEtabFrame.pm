b318d3ef43209c6103e904c8ed922fbf2a24f9eb
Christopher Chavez <chrischavez@gmx.us>
2020-12-12 15:49:08 -0600

    Prefer comparing `$^O` to exact strings
    TODO: should some of these check `$w->windowingsystem` instead of `$^O`?

--- IDEtabFrame.pm
+++ IDEtabFrame.pm
@@ -595,7 +595,7 @@ sub endDrag {
 			$contents->wmRelease;
 			# MainWindow needed here because wmReleased widget don't properly inherit from
 			#   Tk::Toplevel
-			$contents->MainWindow::attributes(-toolwindow => 1) if( $^O =~ /mswin32/i);
+			$contents->MainWindow::attributes(-toolwindow => 1) if ($^O eq 'MSWin32');
 			$contents->MainWindow::title($caption);
 			$contents->MainWindow::geometry($geometry);
  			$contents->MainWindow::deiconify;
